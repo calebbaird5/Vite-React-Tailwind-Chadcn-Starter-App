024968af3578b20aa603cf02ec2e02e752d8a17e
Caleb Baird <79653447+calebbaird5@users.noreply.github.com>
2026-07-11 12:21:08 -0600

    maybe fix pwa background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 
 function App() {
+  useEffect(() => {
+    const root = document.documentElement;
+
+    const style = getComputedStyle(root);
+    const backgroundColor = style.getPropertyValue("--background-color");
+    const meta = document.querySelector(
+      'meta[name="apple-mobile-web-app-status-bar-style"]',
+    );
+    if (meta) {
+      meta.setAttribute("content", backgroundColor);
+    } else {
+      const newMeta = document.createElement("meta");
+      newMeta.setAttribute("name", "apple-mobile-web-app-status-bar-style");
+      newMeta.setAttribute("content", backgroundColor);
+      document.head.appendChild(newMeta);
+    }
+  }, []);
   return (
     <div className="flex justify-center items-center h-screen">
       <Button>test button</Button>
